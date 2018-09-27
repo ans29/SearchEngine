9fbacd045c9ef8714caa42e31c1043693a5ea8ce
ans29 <anshul.sharma1995@gmail.com>
2018-09-28 02:10:52 +0530

    implement basic search, Kway:StringBuilder for speed, save and load (title,No_of_Pages), update comparator

--- src/main/java/PQComparator.java
+++ src/main/java/PQComparator.java
@@ -7,10 +7,6 @@ public class PQComparator implements Comparator<Pair<Long, Pair <Scanner, String
     @Override
     public int compare(Pair<Long, Pair<Scanner, String>> o1, Pair<Long, Pair<Scanner, String>> o2)
     {
-        if (o1.getFirst() < o2.getFirst())
-            return -1;
-        if  (o1.getFirst() > o2.getFirst())
-            return 1;
-        return 0;
+        return o1.getFirst().compareTo(o2.getFirst());
     }
 }
